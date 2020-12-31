39240c2ea0dca9bfda2ada93838ea03b1ddd6968
Jurgen <jurgen.gjoncari@gmail.com>
2020-12-31 20:03:26 +0100

    Fix 'matrix.py'

--- python/matrix/matrix.py
+++ python/matrix/matrix.py
@@ -1,12 +1,13 @@
 class Matrix:
 
     def __init__(self, matrix_string):
-        self.matrix_string = matrix_string
-        self.matrix_chars = [row_string.split() for row_string in self.matrix_string.splitlines()]
-        self.matrix_int = [[int(char) for char in self.matrix_chars[row]] for row in range(len(self.matrix_chars))]
+        self.matrix = [
+                [int(char) for char in row.split()]
+                for row in matrix_string.splitlines()
+            ]
 
     def row(self, index):
-        return self.matrix_int[index - 1]
+        return self.matrix[index - 1]
 
     def column(self, index):
-        return [self.matrix_int[row][index - 1] for row in range(len(self.matrix_int))]
+        return [row[index - 1] for row in self.matrix]
