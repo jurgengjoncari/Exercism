fec1a9fd10dc10f5970bc842f17452fc11339dc3
Jurgen Gjonçari <jurgen.gjoncari@gmail.com>
2021-03-01 08:02:41 +0100

    Add isogram and refactor leap

--- python/isogram/isogram.py
+++ python/isogram/isogram.py
@@ -1,6 +1,3 @@
 def is_isogram(string):
-    low_alnum = ''.join(s for s in string.lower() if s.isalnum())
-    for c in low_alnum:
-        if low_alnum.count(c) != 1:
-            return False
-    return True
+    low_alnum = [c for c in string.lower() if c.isalnum()]
+    return len(low_alnum) == len(set(low_alnum))
